49fad8b952162a7f24cdbe69de9ae71c9779fc31
Mokhirbek9901 <ismoilovmokhirbek@gmail.com>
2026-09-11 12:29:02 +0500

    Remove double animation on iPhone swipe back

--- lib/navigation_sync.dart
+++ lib/navigation_sync.dart
@@ -7,13 +7,19 @@ import 'browser_history.dart';
 class MuhajeerNavigatorObserver extends NavigatorObserver {
   bool _handlingBrowserPop = false;
   int _ignoredBrowserPops = 0;
+  final List<Route<dynamic>> _routeStack = <Route<dynamic>>[];
 
   bool _managed(Route<dynamic> route) =>
       (route.settings.name ?? '').startsWith('mb:');
 
+  Route<dynamic>? get _topRoute =>
+      _routeStack.isEmpty ? null : _routeStack.last;
+
   @override
   void didPush(Route<dynamic> route, Route<dynamic>? previousRoute) {
     super.didPush(route, previousRoute);
+    _routeStack.remove(route);
+    _routeStack.add(route);
     if (_managed(route) && browserHistorySupported) {
       pushBrowserHistoryEntry();
     }
@@ -22,9 +28,41 @@ class MuhajeerNavigatorObserver extends NavigatorObserver {
   @override
   void didPop(Route<dynamic> route, Route<dynamic>? previousRoute) {
     super.didPop(route, previousRoute);
+    _routeStack.remove(route);
+    if (previousRoute != null && !_routeStack.contains(previousRoute)) {
+      _routeStack.add(previousRoute);
+    }
     if (_managed(route) && browserHistorySupported && !_handlingBrowserPop) {
       _ignoredBrowserPops += 1;
       backBrowserHistoryEntry();
+    }
+  }
+
+  @override
+  void didRemove(Route<dynamic> route, Route<dynamic>? previousRoute) {
+    super.didRemove(route, previousRoute);
+    _routeStack.remove(route);
+    if (previousRoute != null && !_routeStack.contains(previousRoute)) {
+      _routeStack.add(previousRoute);
+    }
+  }
+
+  @override
+  void didReplace({Route<dynamic>? newRoute, Route<dynamic>? oldRoute}) {
+    super.didReplace(newRoute: newRoute, oldRoute: oldRoute);
+    if (oldRoute == null) {
+      if (newRoute != null) _routeStack.add(newRoute);
+      return;
+    }
+    final index = _routeStack.indexOf(oldRoute);
+    if (index >= 0) {
+      if (newRoute == null) {
+        _routeStack.removeAt(index);
+      } else {
+        _routeStack[index] = newRoute;
+      }
+    } else if (newRoute != null) {
+      _routeStack.add(newRoute);
     }
   }
 
@@ -38,7 +76,18 @@ class MuhajeerNavigatorObserver extends NavigatorObserver {
 
     _handlingBrowserPop = true;
     try {
-      navigator.pop();
+      final route = _topRoute;
+
+      // iPhone/Safari chapdan swipe qilganda brauzerning o‘zi allaqachon
+      // orqaga qaytish animatsiyasini ko‘rsatadi. Shu payt Navigator.pop()
+      // ishlatilsa Flutter ham ikkinchi reverse animatsiyani bajaradi va sahifa
+      // "yangilangandek" lipillaydi. Managed storefront route'ni darhol olib
+      // tashlash browser gesture'dan keyingi ikkinchi animatsiyani yo‘q qiladi.
+      if (route != null && _managed(route)) {
+        navigator.removeRoute(route);
+      } else {
+        navigator.pop();
+      }
       await Future<void>.delayed(Duration.zero);
     } finally {
       _handlingBrowserPop = false;

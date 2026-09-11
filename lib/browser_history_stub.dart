aeac3dbcf0bc11d689f22daeb1737ff572596b07
Mokhirbek9901 <ismoilovmokhirbek@gmail.com>
2026-09-11 18:36:41 +0500

    Keep browser history API consistent

--- lib/browser_history_stub.dart
+++ lib/browser_history_stub.dart
@@ -3,4 +3,5 @@ import 'dart:async';
 bool get browserHistorySupported => false;
 Stream<void> get browserPopEvents => const Stream<void>.empty();
 void pushBrowserHistoryEntry() {}
+void restoreBrowserHistoryGuard() {}
 void backBrowserHistoryEntry() {}

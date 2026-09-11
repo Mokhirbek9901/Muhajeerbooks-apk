2cf00efb4f9fc793a91eea1fc82e6726d383f9ef
Mokhirbek9901 <ismoilovmokhirbek@gmail.com>
2026-09-11 10:35:53 +0500

    Add browser history stub

---
+++ lib/browser_history_stub.dart
@@ -0,0 +1,6 @@
+import 'dart:async';
+
+bool get browserHistorySupported => false;
+Stream<void> get browserPopEvents => const Stream<void>.empty();
+void pushBrowserHistoryEntry() {}
+void backBrowserHistoryEntry() {}

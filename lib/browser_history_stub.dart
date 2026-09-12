223620a14a49f2620021fc6d088354e83732b388
Mokhirbek9901 <ismoilovmokhirbek@gmail.com>
2026-09-12 12:09:47 +0500

    Add web swipe state stub

--- lib/browser_history_stub.dart
+++ lib/browser_history_stub.dart
@@ -2,6 +2,7 @@ import 'dart:async';
 
 bool get browserHistorySupported => false;
 Stream<void> get browserPopEvents => const Stream<void>.empty();
+void setBrowserBackSwipeEnabled(bool enabled) {}
 void pushBrowserHistoryEntry() {}
 void restoreBrowserHistoryGuard() {}
 void backBrowserHistoryEntry() {}

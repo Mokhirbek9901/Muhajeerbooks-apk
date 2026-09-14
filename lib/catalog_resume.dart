cd941b9b6864889992c647f55064ebdb4f8c1d33
Mokhirbek9901 <ismoilovmokhirbek@gmail.com>
2026-09-15 02:40:01 +0900

    Reset storefront after 30 seconds away

--- lib/catalog_resume.dart
+++ lib/catalog_resume.dart
@@ -10,7 +10,7 @@ class CatalogResume extends ChangeNotifier with WidgetsBindingObserver {
   final DateTime Function() _now;
   static final instance = CatalogResume();
   static const awayKey = 'catalog:awayAt';
-  static const timeout = Duration(minutes: 5);
+  static const timeout = Duration(seconds: 30);
   DateTime? _awayAt;
   late SharedPreferences _prefs;
 
